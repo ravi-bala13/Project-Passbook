2b772068285f1a74ab5673770ce23ab851a0f045
Balamurugan <balamurugan@MyGates-MacBook-Pro.local>
2023-01-17 19:53:24 +0530

    config utils and setup for mongodb integrated

--- src/main/java/com/passbook/demo/PassbookApplication.java
+++ src/main/java/com/passbook/demo/PassbookApplication.java
@@ -4,8 +4,10 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 @Log4j2
@@ -20,6 +22,11 @@ public class PassbookApplication {
 		return args -> {
 			log.info("=====================  Application started =====================");
 		};
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void runThisAfterApplicationStarted(){
+		log.info("working");
 	}
 
 }
---
+++ src/main/java/com/passbook/demo/utils/ConfigUtils.java
@@ -0,0 +1,13 @@
+package com.passbook.demo.utils;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+
+@Getter
+@Component
+public class ConfigUtils {
+
+
+}
