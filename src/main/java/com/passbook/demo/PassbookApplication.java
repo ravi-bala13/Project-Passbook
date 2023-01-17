bab9465beeab662caf297233f834c577283eb33d
Balamurugan <balamurugan@MyGates-MacBook-Pro.local>
2023-01-17 14:00:15 +0530

    maven support added

--- src/main/java/com/passbook/demo/PassbookApplication.java
+++ src/main/java/com/passbook/demo/PassbookApplication.java
@@ -1,13 +1,25 @@
 package com.passbook.demo;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@Log4j2
 public class PassbookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PassbookApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(final ApplicationContext ctx) {
+		return args -> {
+			log.info("=====================  Application started =====================");
+		};
 	}
 
 }
