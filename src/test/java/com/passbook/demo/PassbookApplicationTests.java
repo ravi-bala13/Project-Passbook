1ceff7a6274cd0d054dcfa8a3124559f5397c7a7
Balamurugan <balamurugan@MyGates-MacBook-Pro.local>
2023-01-17 13:18:24 +0530

    initial setup

---
+++ src/test/java/com/passbook/demo/PassbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passbook.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
